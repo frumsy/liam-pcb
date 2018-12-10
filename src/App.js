336e051635b17e08c3d355d88c1b3ace8d31b4c2
frumsy <liambritt3@gmail.com>
2018-12-10 13:06:42 -0500

    add profile links on end text event

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Typewriter from 'typewriter-effect';
-import Top from './components/top/Top'
-let p = ()=>{console.log('p test')}
-let loadTop = ()=>{
-        
-}
+import Top from './components/top/Top';
 
 class App extends Component {
+  state = {
+    visible: 'hidden',   
+}
+
+loadTop = (isVisible) =>{
+    console.log('called: ', isVisible);
+    this.setState({visible: isVisible});
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         <Typewriter
           onInit={(typewriter) => {
-            typewriter.typeString('user@liam-pcb.com$./npm start')
-              .callFunction(()=>loadTop())
+            typewriter.typeString('user@liam-pcb.com$./profiles.sh')
+              .callFunction(()=>this.loadTop('visible'))
               .start();
-              }}/>
-        <Top/> 
+        }}/>
+        <Top visible={this.state.visible}/>
         </header>
       </div>
     );
--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -4,13 +4,10 @@ import GithubImage from './github.png';
 import StackOverflowImage from './stackoverflow.png';
 import UpWorkImage from './upwork.png';
 
-var defaultStyle = 
-{       
-}
- 
 var boxStyle={
+    borderRadius: '10px',
     margin: '20px',
-    background: '#7c7c7c',
+    background: '#1a40618c',
     height: '100px',
     width: '300px',
 }
@@ -20,39 +17,40 @@ var imageStyle={
     maxHeight: '100%',
 }
 
-export default class Top extends React.Component {
-  state = {
-    loaded: false,
-  };
-  onLoad = () => this.setState({ loaded: true });
-
-  render() {
-    const { height, children } = this.props,
-      { loaded } = this.state;
- 
+const Top = (props) => {
+    
+    let t = props.visible
+    console.log('t:',t);
     return (
-        <div className="list" style={defaultStyle}>
+        <div className="list" style={{visibility: props.visible}}>
             <Grid>
                 <Row className="show-grid">
                     <Col xs={6} md={4}>
+                    <a href="https://github.com/frumsy">
                         <div className="Github" style={boxStyle}>
                               <Image src={GithubImage} style={imageStyle}rounded />
                         </div>
+                    </a>
                     </Col>
                     <Col xs={6} md={4}>
+                    <a href="https://stackoverflow.com/users/6706654/liam?tab=profile">
                         <div className="StackOverflow" style={boxStyle}>
                             <Image src={StackOverflowImage} style={imageStyle} rounded />
                         </div>
+                    </a>
                     </Col>
                     <Col xsHidden md={4}>
+                    <a href="https://stackoverflow.com/users/6706654/liam?tab=profile">
                         <div className="UpWork" style={boxStyle}>
-                            <Image href='https://www.upwork.com/o/profiles/users/_~0193916a6f17380807/'src={UpWorkImage} style={imageStyle}rounded />
+                            <Image src={UpWorkImage} style={imageStyle}rounded />
                         </div>
+                    </a>
                     </Col>
                 </Row>
             </Grid>
 
         </div>
     );
-  }
 }
+
+export default Top 
